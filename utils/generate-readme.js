f6edd48d676f36ae2e5698598eff56642a0ecd76
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-04-20 16:15:07 -0400

    added directory creation

--- utils/generate-readme.js
+++ utils/generate-readme.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
+        if (!fs.existsSync('./dist')) {
+            fs.mkdirSync('./dist');
+        }
         fs.writeFile('./dist/README.md', fileContent, err => {
             // if there's an error, reject the Promise and send the error to the Promise's .catch() method
             if (err) {
